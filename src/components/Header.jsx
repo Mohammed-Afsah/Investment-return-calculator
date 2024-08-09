6e5c8e93e8e3b714f1e1baac9ead96e4037199b2
Mohammed-Afsah <95960495+Mohammed-Afsah@users.noreply.github.com>
2024-08-09 21:16:49 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+export default function Header() {
+  return (
+    <header id="header">
+   <h1></h1>
+      <h1 className='mainheading'>Investment Return Calculator</h1>
+    </header>
+  );
+}
